d9a2b0f49d5f31a12f8827ee547b6883cc7a9845
Arnav Menon <63289642+arnavmenon@users.noreply.github.com>
2020-12-18 02:47:31 +0530

    Added donor POST routes

--- routes/donor.js
+++ routes/donor.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Charity = require('../models/Charity');
+const Donor= require('../models/Donor');
 
+//router.get('/donor/profile',async(req,res)=>{})
 
 router.get('/charities/all', async(req,res)=>{
     try{
@@ -23,7 +25,7 @@ router.get('/charities/:query', async(req,res)=>{
     try{
         let char;
         let searchText=req.params.query;
-        char = await Charity.find({$or:[{userName:{ $regex: `^${searchText}`, $options: 'i' }},{userName:{ $regex: `^${searchText}`, $options: 'i' }}]});
+        char = await Charity.find({$or:[{userName:{ $regex: `^${searchText}`, $options: 'i' }},{requirements:{$elemMatch:{material:{ $regex: `^${searchText}`, $options: 'i' }}}}]});
         return res
                 .status(200)
                 .json({data: char});
@@ -35,5 +37,60 @@ router.get('/charities/:query', async(req,res)=>{
             .json({message: "Server error: Try again later"});
     }
 })
+
+router.get('/charityPage/:id', async(req,res)=>{
+    try{
+        let char;
+        let chID=req.params.id;
+        char = await Charity.findById({chID});
+        return res
+                .status(200)
+                .json({data: char});
+    }
+    catch(e){
+        console.log(e);
+        return res
+            .status(500)
+            .json({message: "Server error: Try again later"});
+    }
+})
+
+router.post("/charityPage/donate", async (req, res) => {
+  
+    try{
+        let don = req.body;
+        let charID=don.DonatedTo;
+        let donID=don.DonatedBy;
+        let C=Charity.findById({charID});
+
+        //let D=Donor.findById({donID});
+        //let placeholder1=await Charity.findByIdAndUpdate(charID,{$push: {donation: don}});
+        
+        let placeholder1=await Donor.findByIdAndUpdate(donID,{$push: {donation: don}});
+
+        let newReq=C.requirements;
+        for(let i=0;i<=newReq.length;i++){
+            if(newReq.material==don.material){
+                if(newReq.quantity>don.quantity)
+                    newReq.quantity=newReq.quantity-don.quantity;
+                else
+                    newReq.quantity=0;
+                break;
+            }
+        };
+
+        let placeholder2=await Charity.findByIdAndUpdate(charID,{requirements: newReq, $push: {donation: don}});
+
+        return res  
+                .status(200)
+                .json({message: "Donation Successful"});
+    }
+    catch(e){
+        console.log(e);
+        return res
+            .status(500)
+            .json({message: "Server error: Try again later"});
+    }
+  });
 
 module.exports=router;
